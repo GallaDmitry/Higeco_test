0cbb612a70e5db614fd386bc98764ebdb2909d44
Dmitriy <galla.dmitry@gmail.com>
2025-03-21 14:23:10 +0100

    merge

---
+++ lib/views/home_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First page'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Init body'),
+        ),
+      ),
+    );
+  }
+}
